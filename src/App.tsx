1ee71f1fcb730df2f0e9d8f469241107f90d5958
Bruno Oliveira <brunno.oliver7@gmail.com>
2023-03-16 23:11:13 -0300

    adicionando Chart3 e ReactExample

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Chart1 } from "./components/Chart1";
 import { Chart2 } from "./components/Chart2";
+import { Chart3 } from "./components/Chart3";
+import ReactExample from "./components/ReactExample";
 
 const App = () => {
   return (
     <>
       <Chart1 />
       <Chart2 />
+      <Chart3 />
+      <ReactExample />
     </>
   );
 };
